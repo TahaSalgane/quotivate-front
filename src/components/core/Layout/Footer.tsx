f07b582489663419f8b1d5d9302eecdbbd1c87f5
SKaLo06 <taha.00.kech@gmail.com>
2023-03-27 12:17:58 +0000

    update footer

--- src/components/core/Layout/Footer.tsx
+++ src/components/core/Layout/Footer.tsx
@@ -2,6 +2,65 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="px-3">
+            <div className="footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-1">
+                            <h2>telecharger l &apos; application</h2>
+                            <p>telecharger l &apos;application dispo android et ios</p>
+                            <br />
+                            <img src="/photos/application.jpg" width="200px" />
+                        </div>
+                        <div className="col-2">
+                            <img src="/photos/ofppt.jpg" width="180px" />
+                            <p></p>
+                        </div>
+                        <div className="col-3">
+                            <h3></h3>
+                            <ul>
+                                <li>
+                                    {' '}
+                                    <a href="">Tableau des tailles</a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">Politique de retour</a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">Conditions généralesn </a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">d utilisation Politique</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-4">
+                            <h3></h3>
+                            <ul>
+                                <li>
+                                    {' '}
+                                    <a href="">Corporate</a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">À propos de nous</a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">SPS dans le monde</a>
+                                </li>
+                                <li>
+                                    {' '}
+                                    <a href="">contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <b>©2023 Quotivate</b>
         </div>
     );
